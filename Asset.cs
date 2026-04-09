ca956337006519e4ddcb65ff44bcb900a18b1839
Tito Mutethia <titomutethia@gmail.com>
2026-04-09 23:32:31 +0300

    create a new folder and file to define what a 3D asset

---
+++ Asset.cs
@@ -0,0 +1,10 @@
+namespace MyMauiApp.Models;
+
+public class Asset
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; }
+    public string ImageSource { get; set; }
+    public string Category { get; set; }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,23 +1,33 @@
-﻿namespace MyMauiApp;
+﻿﻿using MyMauiApp.Models;
+using System.Collections.ObjectModel;
+
+namespace MyMauiApp;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	public ObservableCollection<Asset> Assets { get; set; } = new();
 
 	public MainPage()
 	{
 		InitializeComponent();
+		LoadAssets();
+		AssetsCollection.ItemsSource = Assets;
 	}
 
-	private void OnCounterClicked(object? sender, EventArgs e)
+	private void LoadAssets()
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		Assets.Add(new Asset { 
+			Name = "Low-Poly Forest Pack", 
+			Category = "Environment", 
+			Price = "$15.00", 
+			ImageSource = "dotnet_bot.png" // Replace with actual blender renders
+		});
+		
+		Assets.Add(new Asset { 
+			Name = "Cyberpunk Character", 
+			Category = "Characters", 
+			Price = "$45.00", 
+			ImageSource = "dotnet_bot.png" 
+		});
 	}
 }
