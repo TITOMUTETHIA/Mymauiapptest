baddd147f54394c363f51cf1f804c6b347273931
Tito Mutethia <titomutethia@gmail.com>
2026-04-17 23:34:00 +0300

    add a Flyout navigation menu to your MAUI app using Shell

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,7 +1,19 @@
-﻿﻿
-	public MainPage()
-	{	InitializeComponent();
+﻿﻿namespace MyMauiApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
     private async void OnAboutButtonClicked(object sender, EventArgs e)
     {
-        await Navao.
+        await Shell.Current.GoToAsync("AboutPage");
+    }
+
+    private async void OnContactButtonClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("ContactPage");
+    }
+}
