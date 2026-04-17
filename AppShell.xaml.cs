8d5a824184935b5d62869847bf221fe0540217d0
Tito Mutethia <titomutethia@gmail.com>
2026-04-17 23:37:59 +0300

    add logout button

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,4 +1,4 @@
-﻿namespace MyMauiApp;
+﻿﻿namespace MyMauiApp;
 
 public partial class AppShell : Shell
 {
@@ -6,4 +6,13 @@ public partial class AppShell : Shell
 	{
 		InitializeComponent();
 	}
+
+    private async void OnLogoutClicked(object sender, EventArgs e)
+    {
+        bool result = await DisplayAlert("Logout", "Are you sure you want to sign out?", "Yes", "No");
+        if (result)
+        {
+            // Add your logout logic here (e.g., clear auth tokens, navigate to Login page)
+        }
+    }
 }
