cc7a7410d62201feb8f9f9d1b9cf8de5d2ef5db9
Tito Mutethia <titomutethia@gmail.com>
2026-06-04 23:54:25 +0300

    version update

--- obj/Debug/net9.0-ios/iossimulator-x64/MyMauiApp.AssemblyInfo.cs
+++ obj/Debug/net9.0-ios/iossimulator-x64/MyMauiApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMauiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3cab853e46160680678f602efaf652816b9c30a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2c73bc449d52e4c437d1551f238ca278da2581d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMauiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMauiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
