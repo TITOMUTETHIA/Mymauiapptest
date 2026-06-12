4f651517e2e1d5bc4322cc55a25dee7b49cb4341
Tito Mutethia <titomutethia@gmail.com>
2026-06-12 15:51:54 +0300

    UI fix

--- src/MyMauiApp/obj/Debug/net10.0-windows10.0.19041.0/win-x64/MyMauiApp.AssemblyInfo.cs
+++ src/MyMauiApp/obj/Debug/net10.0-windows10.0.19041.0/win-x64/MyMauiApp.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMauiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ae51030f333d343bbf4340bc3ee219213dbb112e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+19ed5b95a0789637b879f2104c793f8eb5dbe742")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMauiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMauiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
